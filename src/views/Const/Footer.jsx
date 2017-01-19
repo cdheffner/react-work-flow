dc9cfbfb34cbd7a961d6b6f79f3d7a5d8782cc6f
Joshua <joshuacode16@gmail.com>
2017-01-19 17:24:16 -0600

    Added functional way for rendering view

--- src/views/Const/Footer.jsx
+++ src/views/Const/Footer.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-export default class Footer extends React.Component{
-  render() {
-    return(
-      <div>
-        <footer>
-          <ul>
-            <li><Link to='https://github.com/joshcode16' target="_blank">Joshua</Link></li>
-            <li><Link to='https://github.com/cdheffner' target="_blank">Daniel</Link></li>
-            <li><Link to='https://github.com/colshacol' target="_blank">Colton</Link></li>
-            <li><Link to='https://github.com/denzuko' target="_blank">Spencer</Link></li>
-          </ul>
-        </footer>
-      </div>
-    )
-  }
+//When Adding Logic Use A Class
+// export default class Footer extends React.Component{
+//   render() {
+//     return(
+//       <div>
+//         <footer>
+//           <ul>
+//             <li><Link to='https://github.com/joshcode16' target="_blank">Joshua</Link></li>
+//             <li><Link to='https://github.com/cdheffner' target="_blank">Daniel</Link></li>
+//             <li><Link to='https://github.com/colshacol' target="_blank">Colton</Link></li>
+//             <li><Link to='https://github.com/denzuko' target="_blank">Spencer</Link></li>
+//           </ul>
+//         </footer>
+//       </div>
+//     )
+//   }
+// }
+
+//Functional Way Adding Logic Is Optional
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <ul>
+          <li><Link to='https://github.com/joshcode16' target="_blank">Joshua</Link></li>
+          <li><Link to='https://github.com/cdheffner' target="_blank">Daniel</Link></li>
+          <li><Link to='https://github.com/colshacol' target="_blank">Colton</Link></li>
+          <li><Link to='https://github.com/denzuko' target="_blank">Spencer</Link></li>
+        </ul>
+      </footer>
+    </div>
+  )
 }
